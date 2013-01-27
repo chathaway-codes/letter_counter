ea61f470cb407c8ba14ed653bf905c248b31dbb0
Charles H <chathaway@logrit.com>
2013-01-26 21:15:39 -0500

    Fixed a few bugs

--- html/js/app.js
+++ html/js/app.js
@@ -1,7 +1,7 @@
 $.jqplot.config.enablePlugins = true;
 
 function countChars(text) {
-  var out = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0, "A": 0, "B": 0, "C": 0, "D": 0, "E": 0, "F": 0, "G": 0, "H": 0, "I": 0, "J": 0, "K": 0, "L": 0, "M": 0, "N": 0, "O": 0, "P": 0, "Q": 0, "R": 0, "S": 0, "T": 0, "U": 0, "V": 0, "W": 0, "X": 0, "Y": 0, "Z": 0}
+  var out = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0, "A": 0, "B": 0, "C": 0, "D": 0, "E": 0, "F": 0, "G": 0, "H": 0, "I": 0, "J": 0, "K": 0, "L": 0, "M": 0, "N": 0, "O": 0, "P": 0, "Q": 0, "R": 0, "S": 0, "T": 0, "U": 0, "V": 0, "W": 0, "X": 0, "Y": 0, "Z": 0};
   for(var c in text) {
     if(text[c] in out)
       out[text[c]] += 1;
@@ -21,12 +21,20 @@ function countChars(text) {
 function caesarShift(text, amount) {
   var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   
+  var alpabet_map = {};
+  
+  for(var k=0; k < alphabet.length; k++) {
+    alpabet_map[alphabet[k]] = k;
+  }
+  
   var out = "";
   
-  for(var k in text) {
-    var index = alphabet.indexOf(text[k]);
-    if(index != -1)
-      out += alphabet[(index + amount) % alphabet.length];
+  for(var k=0; k < text.length; k++) {
+    var index = alpabet_map[text[k]];
+    if(index !== undefined)
+      out += alphabet[((index + amount) % (alphabet.length+1))];
+    else
+      out += text[k];
   }
   
   return out;
@@ -81,7 +89,12 @@ var App = Backbone.View.extend({
   plot2: null,
   
   makeGraph: function() {
-    console.log("makeGraph");
+    // If plot1 and plot2 exist, free them
+    this.plot1 = null;
+    this.plot2 = null;
+    $("#raw_graph").empty();
+    $("#caesar_graph").empty();
+    
     
     var text = this.$el.find("#raw").val();
     
